6ea426f48becb848335e93a83d75ce5b6cccb52b
dencheva <dencheva@stsci.edu>
2003-10-09 15:26:49 +0000

    Adding support for --home and --prefix
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/numdisplay@8 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
-import os 
+import os, sys, string, os.path
 from distutils.core import setup
 from distutils.sysconfig import *
 from distutils.command.install import install
 
 
-py_bin = parse_makefile(get_makefile_filename())['BINDIR']
+#params = string.join(sys.argv[2:])
 ver = sys.version_info
-python_exec = 'python' + str(ver[0])+'.'+str(ver[1])
-params = string.join(sys.argv[2:])
-                     
-if __name__ == '__main__' :
+python_exec = 'python' + str(ver[0])+ '.' + str(ver[1])
 
+def getDataDir(args):
+    for a in args:
+        if string.find(a, '--home=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib/python/numdisplay')
+        elif string.find(a, '--prefix=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/numdisplay')
+        elif string.find(a, '--install-data=') == 0:
+            dir = string.split(a, '=')[1]
+            data_dir = dir
+        else:
+            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/numdisplay')
+    return data_dir
+                
+
+
+
+if __name__ == '__main__' :
+    args = sys.argv
+    data_dir = getDataDir(args)
     setup(
         name="Numdisplay",
         version="0.1",
@@ -22,7 +40,6 @@ if __name__ == '__main__' :
         url="",
         packages = ['numdisplay'],
 	package_dir={'numdisplay':''},
-	extra_package_dir = ['numdisplay'],
-	data_files = [('numdisplay', ['imtoolrc'])]
+	data_files = [(data_dir, ['imtoolrc'])]
         )
 
