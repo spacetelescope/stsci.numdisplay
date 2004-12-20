aa42e0f2c6411cbe1a7c767a3dc47b84ce235f3b
iraf <iraf@stsci.edu>
2004-12-20 20:47:58 +0000

    Checking in released version of setup
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/numdisplay@230 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import os, sys, string, os.path
+import sys, string, os.path 
 from distutils.core import setup
 from distutils.sysconfig import *
 from distutils.command.install import install
-
+import shutil
 
 ver = sys.version_info
 python_exec = 'python' + str(ver[0])+ '.' + str(ver[1])
@@ -24,15 +24,44 @@ def getDataDir(args):
             data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/numdisplay')
     return data_dir
                 
+def copy_doc(data_dir, args):
+    if 'install' in args:
+        doc_dir = os.path.join(data_dir,'doc')
+        if os.path.exists(doc_dir):
+	    try:
+                shutil.rmtree(doc_dir)
+            except:
+                print "Error removing doc directory\n"
+	shutil.copytree('doc', doc_dir)
 
+
+
+def dolocal():
+    """Adds a command line option --local=<install-dir> which is an abbreviation for
+    'put all of numdisplay in <install-dir>/numdisplay'."""
+    if "--help" in sys.argv:
+        print >>sys.stderr
+        print >>sys.stderr, " options:"
+        print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir>"
+    for a in sys.argv:
+        if a.startswith("--local="):
+            dir =  os.path.abspath(a.split("=")[1])
+            sys.argv.extend([
+                "--install-lib="+dir,
+                "--install-data="+os.path.join(dir,"numdisplay")
+                ])
+            sys.argv.remove(a)
 
 
 if __name__ == '__main__' :
     args = sys.argv
+    print "numdisplay", args
+    dolocal()
     data_dir = getDataDir(args)
+
     setup(
         name="Numdisplay",
-        version="0.1",
+        version="1.0",
         description="Package for displaying numarray arrays in DS9",
         author="Warren Hack",
         maintainer_email="help@stsci.edu",
@@ -44,3 +73,4 @@ if __name__ == '__main__' :
 	data_files = [(data_dir, ['imtoolrc'])]
         )
 
+    copy_doc(data_dir, args)
