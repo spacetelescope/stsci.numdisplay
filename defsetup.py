8d27f8e7e82e275c948e827780f81ddcf54b8f1c
sienkiew <sienkiew@stsci.edu>
2008-08-13 18:29:12 +0000

    setup.py mods - first batch of packages
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/numdisplay@6916 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ defsetup.py
@@ -0,0 +1,14 @@
+pkg = "numdisplay"
+
+setupargs = { 
+    'version' :         "1.5",
+    'description' :     "Package for displaying numpy arrays in DS9",
+    'author' :          "Warren Hack",
+    'maintainer_email': "help@stsci.edu",
+    'url' :             "",
+    'license' :         "http://www.stsci.edu/resources/software_hardware/pyraf/LICENSE",
+    'platforms' :       ["any"],
+    'data_files' :      [ ( pkg, ['lib/imtoolrc', 'lib/LICENSE.txt','lib/ichar.dat'] )]
+    }
+
+
